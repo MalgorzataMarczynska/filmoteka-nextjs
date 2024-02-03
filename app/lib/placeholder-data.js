a0838ff350dd272c012ddeaeb39ef30008cf4716
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2024-02-03 20:25:12 +0100

    database connection

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,40 +1,52 @@
-export const users = [
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const users = [
   {
-    id: "410544b2-4001-4271-9855-fec4b654442a",
+    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     name: "Someone",
-    email: "user1@nextmail.com",
+    email: "user11@nextmail.com",
     password: "alv12345alv6",
   },
-];
-export const movies = [
   {
-    userId: "410544b2-4001-4271-9855-fec4b654442a",
+    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    name: "Somebody",
+    email: "user22@nextmail.com",
+    password: "alv12387dnj45",
+  },
+];
+const movies = [
+  {
+    userId: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     movieId: 609681,
     status: "watched",
     date: "2024-01-02",
   },
   {
-    userId: "410544b2-4001-4271-9855-fec4b654442a",
+    userId: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     movieId: 787699,
     status: "queue",
     date: "2024-01-12",
   },
   {
-    userId: "410544b2-4001-4271-9855-fec4b654442a",
+    userId: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     movieId: 792307,
     status: "queue",
     date: "2024-01-15",
   },
   {
-    userId: "410544b2-4001-4271-9855-fec4b654442a",
+    userId: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     movieId: 673593,
     status: "watched",
     date: "2024-01-21",
   },
   {
-    userId: "410544b2-4001-4271-9855-fec4b654442a",
+    userId: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
     movieId: 447365,
     status: "watched",
     date: "2024-01-16",
   },
 ];
+module.exports = {
+  users,
+  movies,
+};
---
+++ scripts/seed.js
@@ -0,0 +1,55 @@
+const { db } = require("@vercel/postgres");
+const { users, movies } = require("../app/lib/placeholder-data.js");
+const bcrypt = require("bcrypt");
+
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    const createTable = await client.sql`CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY, name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL);`;
+    console.log(`Created "users" table`);
+    //console.log("users placeholder", users);
+    const insertedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.sql`INSERT INTO users (id, name, email, password) VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword}) ON CONFLICT (id) DO NOTHING;`;
+      })
+    );
+    console.log(`Seeded ${insertedUsers.length} users`);
+    return { createTable, users: insertedUsers };
+  } catch (error) {
+    console.error("Error seeding users:", error);
+    throw error;
+  }
+}
+async function seedMovies(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    const createTable =
+      await client.sql`CREATE TABLE IF NOT EXISTS movies (userId UUID NOT NULL, movieId INT NOT NULL, status VARCHAR(255) NOT NULL, date DATE NOT NULL);`;
+    console.log(`Created "movies" table`);
+    const insertedMovies = await Promise.all(
+      movies.map(
+        (movie) =>
+          client.sql`INSERT INTO movies (userId, movieId, status, date) VALUES (${movie.userId}, ${movie.movieId}, ${movie.status}, ${movie.date});`
+      )
+    );
+    console.log(`Seeded ${insertedMovies.length} movies`);
+    return { createTable, movies: insertedMovies };
+  } catch (error) {
+    console.error("Error seeding movies:", error);
+    throw error;
+  }
+}
+async function main() {
+  const client = await db.connect();
+  await seedUsers(client);
+  await seedMovies(client);
+
+  await client.end();
+}
+main().catch((err) => {
+  console.error("An error occured while attempting to seed the database:", err);
+});
